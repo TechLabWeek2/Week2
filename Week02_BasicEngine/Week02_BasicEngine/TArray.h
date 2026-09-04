ac5528ec691c3803ebba9e480566f4a07c60e790
taehun01 <kth010910@naver.com>
2026-09-04 19:47:21 +0900

    [feat] RemoveAtSwap() 추가

--- Week02_BasicEngine/Week02_BasicEngine/TArray.h
+++ Week02_BasicEngine/Week02_BasicEngine/TArray.h
@@ -11,7 +11,7 @@ private:
 	int32 dataNum; // data의 item 개수
 	int32 dataMax; // data의 최대 크기
 
-	void AllocateMemory() // data의 공간이 부족한 경우 메모리를 재할당
+	void AllocateMemory () // data의 공간이 부족한 경우 메모리를 재할당
 	{
 		if (data == nullptr)
 		{
@@ -29,7 +29,7 @@ private:
 			dataMax *= 2;
 		}
 	}
-	void IndexCheck(int32 Index) const // input으로 들어온 index가 범위안에 있는지 확인
+	void IndexCheck (int32 Index) const // input으로 들어온 index가 범위안에 있는지 확인
 	{
 		if (Index < 0 || Index >= dataNum)
 		{
@@ -37,17 +37,17 @@ private:
 		}
 	
 	}
-	bool IsEmpty() const // data가 비어있는지 확인
+	bool IsEmpty () const // data가 비어있는지 확인
 	{
 		return dataNum <= 0;
 	}
 
 public:
-	TArray()
+	TArray ()
 		: data(nullptr), dataNum(0), dataMax(DATA_MAX_INIT)
 	{}
 
-	TArray(const TArray& otherArray) // 복사 생성자
+	TArray (const TArray& otherArray) // 복사 생성자
 		: data(nullptr), dataNum(0), dataMax(DATA_MAX_INIT)
 	{
 		if (otherArray.data)
@@ -89,7 +89,7 @@ public:
 		return *this;
 	}
 
-	~TArray()
+	~TArray ()
 	{
 		delete[] data;
 	}
@@ -106,24 +106,24 @@ public:
 		return data[Index];
 	}
 
-	int32 Num() const // data의 Item 개수를 반환
+	int32 Num () const // data의 Item 개수를 반환
 	{
 		return dataNum;
 	}
 
-	int32 Max() const // data의 최대 크기를 반환
+	int32 Max () const // data의 최대 크기를 반환
 	{
 		return dataMax;
 	}
 
-	void Add(T Item) // Item을 data에 추가
+	void Add (T Item) // Item을 data에 추가
 	{
 		AllocateMemory();
 
 		data[dataNum++] = Item;
 	}
 
-	void Insert(T Item, int32 Index) // Item을 특정 Index에 추가
+	void Insert (T Item, int32 Index) // Item을 특정 Index에 추가
 	{
 		if (Index < 0 || Index > dataNum)
 		{
@@ -139,7 +139,7 @@ public:
 		dataNum++;
 	}
 
-	T Pop() // 마지막 Item을 Pop
+	T Pop () // 마지막 Item을 Pop
 	{
 		if (IsEmpty())
 		{
@@ -152,7 +152,7 @@ public:
 		return poppedValue;
 	}
 
-	void RemoveAt(int32 Index) // 특정 Index의 Item을 제거
+	void RemoveAt (int32 Index) // 특정 Index의 Item을 제거
 	{
 		if (IsEmpty())
 		{
@@ -167,7 +167,18 @@ public:
 		dataNum--;
 	}
 
-	int32 Find(const T Item) const // 첫번째로 찾은 Item의 Index를 반환. 없으면 -1 반환
+	void RemoveAtSwap(int32 Index) // 특정 Index의 Item을 tail 원소와 swap으로 제거 O(1)
+	{
+		if (IsEmpty())
+		{
+			throw std::out_of_range("RemoveAtSwap() on empty data");
+		}
+		IndexCheck(Index);
+		data[Index] = data[dataNum - 1];
+		dataNum--;
+	}
+
+	int32 Find (const T Item) const // 첫번째로 찾은 Item의 Index를 반환. 없으면 -1 반환
 	{
 		for (int32 i = 0; i < dataNum; i++)
 		{
@@ -179,7 +190,7 @@ public:
 		return -1;
 	}
 
-	void Remove(const T Item) // 첫번째로 찾은 Item을 제거
+	void Remove (const T Item) // 첫번째로 찾은 Item을 제거
 	{
 		int32 foundIndex = Find(Item);
 
