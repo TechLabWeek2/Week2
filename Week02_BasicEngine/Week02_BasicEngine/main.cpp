91205cd529e99dd7c22e7956d56239e7919b8237
taehun01 <kth010910@naver.com>
2026-09-04 19:16:53 +0900

    [feat] TArray 구현 완료

--- Week02_BasicEngine/Week02_BasicEngine/main.cpp
+++ Week02_BasicEngine/Week02_BasicEngine/main.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
+#include "CoreType.h"
 #include "TArray.h"
 
 int main()
 {
-	TArray<int> test;
-	test.Add(34);
-	test.Add(130);
-	std::cout << test[1] << std::endl;
 	return 0;
 }
