325ef8e9edfd2bc034d72d673e443e2894659b79
조혁 <56536360+HyeokJo@users.noreply.github.com>
2026-09-05 10:39:56 +0900

    [Fix] Pull Request 전에 main 함수 내용 제거

--- Week02_BasicEngine/Week02_BasicEngine/main.cpp
+++ Week02_BasicEngine/Week02_BasicEngine/main.cpp
@@ -1,28 +1,6 @@
 #include <iostream>
-#include "CoreType.h"
-#include "TArray.h"
-#include "TSet.h"
-#include "TMap.h"
-
-#include "FString.h"
 
 int main()
 {
-
-	int32 a = 3;
-	int32 b = 5;
-
-	std::cout << a + b << std::endl;
-
-	//FString(L"Test");
-
-	FString F1(L"Test1 ");
-	F1 += L"Test2";
-
-	std::wcout << F1 << std::endl;
-
-	std::cout << F1.Find(L"e", 1) << std::endl;
-
-
 	return 0;
 }
