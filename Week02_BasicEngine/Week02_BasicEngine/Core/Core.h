d7644f7a0b8dc852641793246209c5e09eaa6ef8
조혁 <56536360+HyeokJo@users.noreply.github.com>
2026-09-05 21:59:56 +0900

    [Feat] FVector 클래스 분리, Core.h 생성
    FString, TArray 등등 추가하고 있는 Core.h 생성

---
+++ Week02_BasicEngine/Week02_BasicEngine/Core/Core.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "FVector.h"
+#include "FString.h"
+#include "TArray.h"
+#include "Utils/Math.hpp"
+#include "CoreType.h"
--- Week02_BasicEngine/Week02_BasicEngine/URenderer.h
+++ Week02_BasicEngine/Week02_BasicEngine/URenderer.h
@@ -15,82 +15,13 @@
 #include "ImGui/imgui_impl_dx11.h"
 #include "imGui/imgui_impl_win32.h"
 
-#include "../Week02_BasicEngine/Utils/Math.hpp"
+#include "Core/Core.h"
 
 #include <math.h>
 struct FVertexSimple
 {
     float x, y, z;    // Position
     float r, g, b, a; // Color
-};
-
-class FVector {
-public:
-    float x, y, z;
-    FVector(float _x = 0, float _y = 0, float _z = 0) : x(_x), y(_y), z(_z) {}
-    void Normalize() {
-        float d = x * x + y * y + z * z;
-        if (d == 1) {
-            return;
-        }
-        if (d < (1.0E-8F)) {
-            return;
-        }
-        d = sqrt(d);
-        x = x / d;
-        y = y / d;
-        z = z / d;
-    }
-    FVector Cross(FVector v1, FVector v2) {
-        FVector cross;
-        cross.x = v1.y * v2.z - v1.z * v2.y;
-        cross.y = v1.z * v2.x - v1.x * v2.z;
-        cross.z = v1.x * v2.y - v1.y * v2.x;
-
-        return cross;
-    }
-    float Dot(FVector v1, FVector v2) {
-        float dot = v1.x* v2.x + v1.y * v2.y + v1.z * v2.z;
-
-        return dot;
-    }
-    FVector RotationFromAxes(
-        const FVector& XAxis,
-        const FVector& YAxis,
-        const FVector& ZAxis)
-    {
-        FMatrix4x4 R;
-
-        R.m[0][0] = XAxis.x;
-        R.m[0][1] = XAxis.y;
-        R.m[0][2] = XAxis.z;
-
-        R.m[1][0] = YAxis.x;
-        R.m[1][1] = YAxis.y;
-        R.m[1][2] = YAxis.z;
-
-        R.m[2][0] = ZAxis.x;
-        R.m[2][1] = ZAxis.y;
-        R.m[2][2] = ZAxis.z;
-
-        float angleY = std::asin(-R.m[2][0]);
-
-        float angleX = std::atan2(
-            R.m[2][1],
-            R.m[2][2]
-        );
-
-        float angleZ = std::atan2(
-            R.m[1][0],
-            R.m[0][0]
-        );
-
-        return {
-            (angleX* 57.3f),
-            (angleY* 57.3f),
-            (angleZ* 57.3f)
-        };
-    }
 };
 
 struct alignas(16) FConstants {
