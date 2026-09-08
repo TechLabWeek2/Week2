9604a5f79589a9e92d4ed92f540615611a0e1437
조혁 <56536360+HyeokJo@users.noreply.github.com>
2026-09-08 19:43:11 +0900

    [Fix] 필요없는 주석 제거

--- Week02_BasicEngine/Week02_BasicEngine/Shapes.h
+++ Week02_BasicEngine/Week02_BasicEngine/Shapes.h
@@ -17,54 +17,6 @@ struct ResourceData
 	D3D11_PRIMITIVE_TOPOLOGY Topology = D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
 };
 
-//extern TArray<FVertexSimple> line_vertices;
-//
-//extern TArray<FVertexSimple> triangle_vertices;
-//
-//extern TArray<FVertexSimple> plane_vertices;
-//
-//extern TArray<FVertexSimple> cube_vertices;
-//
-//extern TArray<FVertexSimple> sphere_vertices;
-//extern FVertexSimple sphere_vertices[2400];
-//
-//extern TArray<FVertexSimple> location_gizmo_vertices;
-//extern TArray<FVertexSimple> scale_gizmo_vertices;
-//extern TArray<FVertexSimple> rotation_gizmo_vertices;
-//
-//extern FVertexSimple location_gizmo_x_vertices[144];
-//extern FVertexSimple location_gizmo_y_vertices[144];
-//extern FVertexSimple location_gizmo_z_vertices[144];
-//
-//extern FVertexSimple scale_gizmo_x_vertices[93];
-//extern FVertexSimple scale_gizmo_y_vertices[93];
-//extern FVertexSimple scale_gizmo_z_vertices[93];
-//
-//extern FVertexSimple rotation_gizmo_x_vertices[1536];
-//extern FVertexSimple rotation_gizmo_y_vertices[1536];
-//extern FVertexSimple rotation_gizmo_z_vertices[1536];
-
-//extern TArray<FVertexSimple> cube_vertices;
-//extern FVertexSimple floor1_vertices[12];
-//extern FVertexSimple floor2_vertices[12];
-//
-//extern FVertexSimple cube_vertices[36];
-//
-//extern TArray<FVertexSimple> sphere_vertices;
-//extern FVertexSimple sphere_vertices[2400];
-//
-//extern FVertexSimple location_gizmo_x_vertices[144];
-//extern FVertexSimple location_gizmo_y_vertices[144];
-//extern FVertexSimple location_gizmo_z_vertices[144];
-//
-//extern FVertexSimple scale_gizmo_x_vertices[93];
-//extern FVertexSimple scale_gizmo_y_vertices[93];
-//extern FVertexSimple scale_gizmo_z_vertices[93];
-//
-//extern FVertexSimple rotation_gizmo_x_vertices[1536];
-//extern FVertexSimple rotation_gizmo_y_vertices[1536];
-//extern FVertexSimple rotation_gizmo_z_vertices[1536];
-
 extern ResourceData LineResource;
 extern ResourceData TriangleResource;
 extern ResourceData PlaneResource;
