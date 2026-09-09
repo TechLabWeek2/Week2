b67c78a898b56a4ee397989a2911b42499ac0a72
김민곤 <minggong222@naver.com>
2026-09-10 00:32:53 +0900

    [Help] 기즈모 바라보는 방향에 따라 반대가 됩니다
    회전을 많이 하고 이동을 할때 오류가 있습니다

--- Week02_BasicEngine/Week02_BasicEngine/UGizmo.cpp
+++ Week02_BasicEngine/Week02_BasicEngine/UGizmo.cpp
@@ -100,14 +100,57 @@ void UGizmo::ObjUpdate(UPrimitiveComponent* Obj, FVector MouseMove, POINT Curren
                 LocalGizmoAxis = RelativeQ.RotateVector(FVector(1, 0, 0));
                 break;
             case ETypeAxis::YAxis:
-                LocalGizmoAxis = (RelativeQ * RelativeQ.FromEuler(FVector(0, 0, PI / 2))).RotateVector(FVector(0, 1, 0));
+                LocalGizmoAxis = (RelativeQ * RelativeQ.FromEuler(FVector(0, 0, -PI / 2))).RotateVector(FVector(0, 1, 0));
                 break;
             case ETypeAxis::ZAxis:
-                LocalGizmoAxis = (RelativeQ * RelativeQ.FromEuler(FVector(0, -PI / 2, 0))).RotateVector(FVector(0, 0, 1));
+                LocalGizmoAxis = (RelativeQ * RelativeQ.FromEuler(FVector(0, PI / 2, 0))).RotateVector(FVector(0, 0, 1));
                 break;
             }
-            float Distance = MouseMove.Dot(LocalGizmoAxis);
-            Obj->RelativeLocation += LocalGizmoAxis * Distance;
+
+
+            // 2. 기즈모 원점과 끝점
+            FVector Origin = Obj->RelativeLocation;
+            FVector End = Origin + LocalGizmoAxis * 1.25f;
+
+
+            // 3. 두 점을 Clip Space로 변환
+            FVector OriginClip =
+                Origin *
+                Obj->GetQuatModelMatrix() *
+                Camera->GetViewMatrix() *
+                Camera->GetProjectionMatrix();
+
+            FVector EndClip =
+                End *
+                Obj->GetQuatModelMatrix() *
+                Camera->GetViewMatrix() *
+                Camera->GetProjectionMatrix();
+
+
+            // 5. 화면상의 기즈모 축 방향
+            FVector2 ScreenAxis = FVector2(EndClip.x / EndClip.z - OriginClip.x / OriginClip.z, EndClip.y / EndClip.z - OriginClip.y / OriginClip.z);
+
+            ScreenAxis.Normalize();
+
+
+            // 6. 마우스 이동을 화면상의 기즈모 축으로 투영
+            FVector2 MouseDelta(CurrentMousePos.x - LastMousePos.x, -CurrentMousePos.y + LastMousePos.y);
+            
+            float Distance = MouseDelta.Dot(ScreenAxis);
+
+
+            // 7. 화면 이동량을 월드 이동량으로 변환
+            float WorldPerPixel = 0.005f;
+            FQuat InverseQ = FQuat(
+                -RelativeQ.x,
+                -RelativeQ.y,
+                -RelativeQ.z,
+                RelativeQ.w
+            );
+
+            FVector LocalMove =InverseQ.RotateVector(LocalGizmoAxis * Distance * WorldPerPixel);
+
+            Obj->RelativeLocation += LocalGizmoAxis * Distance * WorldPerPixel;
             break;
         }
         case Rotation: {
@@ -123,6 +166,8 @@ void UGizmo::ObjUpdate(UPrimitiveComponent* Obj, FVector MouseMove, POINT Curren
                 LocalGizmoAxis = (RelativeQ * RelativeQ.FromEuler(FVector(0, 0, 0))).RotateVector(FVector(0, 0, 1));
                 break;
             }
+            float reverse = LocalGizmoAxis.Dot(Camera->GetForwardVector_UE());
+            
             float Angle = 0.0f;
             FQuat DeltaQuat;
             FVector CurrentObjPoint = Obj->RelativeLocation * Obj->GetQuatModelMatrix() * Camera->GetViewMatrix() * Camera->GetProjectionMatrix();
@@ -149,7 +194,8 @@ void UGizmo::ObjUpdate(UPrimitiveComponent* Obj, FVector MouseMove, POINT Curren
             float Dot = LastX * CurrentX + LastY * CurrentY;
 
             Angle = atan2(CurrentY, CurrentX) - atan2(LastY, LastX);
-
+            
+            
             switch (Axis)
             {
             case XAxis:
@@ -164,11 +210,16 @@ void UGizmo::ObjUpdate(UPrimitiveComponent* Obj, FVector MouseMove, POINT Curren
             default:
                 break;
             }
+            if (reverse < 0) DeltaQuat.Inverse();
+            DeltaQuat.Normalize();
             Obj->RelativeQ = Obj->RelativeQ * DeltaQuat;
             break;
         }
         case Scale: {
             Obj->RelativeScale3D += MouseMove;
+            Obj->RelativeScale3D.x = max(0, Obj->RelativeScale3D.x);
+            Obj->RelativeScale3D.y = max(0, Obj->RelativeScale3D.y);
+            Obj->RelativeScale3D.z = max(0, Obj->RelativeScale3D.z);
             break;
         }
         default:
@@ -231,6 +282,9 @@ void UGizmo::ObjUpdate(UPrimitiveComponent* Obj, FVector MouseMove, POINT Curren
         }
         case ETypeTransform::Scale:
             Obj->RelativeScale3D += MouseMove;
+            Obj->RelativeScale3D.x = max(0, Obj->RelativeScale3D.x);
+            Obj->RelativeScale3D.y = max(0, Obj->RelativeScale3D.y);
+            Obj->RelativeScale3D.z = max(0, Obj->RelativeScale3D.z);
             break;
         }
         break;
